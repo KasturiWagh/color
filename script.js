4be6c20c266fb25182e15bd6f8ffb798aab8fc96
Kasturi Wagh <88839908+KasturiWagh@users.noreply.github.com>
2021-08-27 00:27:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const color=[ '#F08D7E', '#BEFCFF' , '#ACAEC5' , '#DEFFFA','#FFDAF5' , '#B0E1FF' , '#E6C6FF' , '#97B299' , '#EFBA97' , '#BE8CE5'];
+function function1() {
+            document.body.style.background = color[Math.floor(Math.random()*10)];
+    }
